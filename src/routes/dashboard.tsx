be453a39b194e60b7bc7444ab208797056b9aa3f
palomaprates <paloma.prates108@gmail.com>
2026-01-27 18:09:34 +0000

    updating dashboard layout

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
-import { Dropzone } from '../components/Dropzone'
 import { AppSidebar } from '@/components/sidebarComponents/AppSidebar'
 import { SidebarProvider } from '@/components/ui/sidebar'
+import { Dropzone } from '@/components/dropzoneComponents/Dropzone'
 
 export const Route = createFileRoute('/dashboard')({
   beforeLoad: ({ context }) => {
@@ -19,22 +19,12 @@ export const Route = createFileRoute('/dashboard')({
 function Dashboard() {
   return (
 <SidebarProvider>
-<div className="w-full px-4 md:px-8 h-[calc(100vh-56px)] flex gap-8 overflow-y-hidden">
-  <div className="w-64 h-full flex flex-col">
+<div className="min-w-screen px-4 md:px-8 flex gap-8 bg-violet-50 min-h-screen">
+  <div className="w-64 flex flex-col">
     <AppSidebar />
   </div>
   <div className="flex-1 flex justify-around flex-col items-center">
-      <div className="w-full h-full flex flex-col justify-center items-center">
-    <section className="w-full min-h-[400px] flex flex-col justify-center items-center bg-gradient-to-r from-blue-300 to-purple-300
- p-10 rounded-3xl border border-slate-800 shadow-2xl backdrop-blur-md hover:scale-105 transition-all">
-      <div className="mb-8">
-        <h1 className="text-slate-800 text-2xl font-sans font-extrabold">
-          Choose documents to start the AI chat
-        </h1>
-      </div>
-      <Dropzone />
-    </section>
-  </div>
+    <Dropzone />
   </div>
 </div>
 </SidebarProvider>
